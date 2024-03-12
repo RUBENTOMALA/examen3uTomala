2569d85ca95e0b665089bbf211a9d6b688916e9c
RUBEN ALFREDO TOMALA VERA <ruben.al97@hotmail.com>
2024-03-11 20:10:44 -0500

    COmmit de las 20:10

---
+++ productos/src/main/java/ec/edu/espe/tercerparcial/controller/ProductoController.java
@@ -0,0 +1,4 @@
+package ec.edu.espe.tercerparcial.controller;
+
+public class ProductoController {
+}
--- productos/src/main/java/ec/edu/espe/tercerparcial/service/ProductoService.java
+++ productos/src/main/java/ec/edu/espe/tercerparcial/service/ProductoService.java
@@ -1,6 +1,7 @@
 package ec.edu.espe.tercerparcial.service;
 
 import ec.edu.espe.tercerparcial.dao.ProductoRepository;
+import ec.edu.espe.tercerparcial.domain.Producto;
 
 public class ProductoService {
     private final ProductoRepository productoRepository;
@@ -10,10 +11,27 @@ public class ProductoService {
     }
 
     public Producto buscarPorId(String codigoUnico){
-        Producto cliente = this.productoRepository.findByCodigoUnico(codigoUnico);
+        Producto producto = this.productoRepository.findByCodigoUnico(codigoUnico);
         if (cliente != null) {
+            return  producto;
+        }
+        else {
+            throw new RuntimeException("No se ha podido encontrar el producto con id: " + codigoUnico);
+        }
+    }
 
-        return;
+    public Producto crearProducto(Producto producto){
+        return this.productoRepository.save(producto);
+    }
+
+    public void modificarProducto(Producto productoActualizado){
+        Producto productoAux = this.productoRepository.findByCodigoUnico(productoActualizado.getCodigoUnico());
+
+        if (productoAux!=null){
+            productoAux.setPrecio(productoActualizado.getPrecio());
+            productoAux.setNombre(productoActualizado.getNombre());
+            productoAux.setDescripcion(productoActualizado.getDescripcion());
+        }
 
     }
 }
